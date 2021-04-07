6ecd86af758db14446e8dcab4a31c4bc5d0af1ec
Nash <72932477+Nashwalt@users.noreply.github.com>
2021-04-06 23:58:16 -0400

    Connect database to app.

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true 
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/nameless-taiga-04904',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
